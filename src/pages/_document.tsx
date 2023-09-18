588358e62ebf0072332914f1f483497fc83df1dc
ysk0423 <ssyhrsysk23@gmail.com>
2023-09-18 15:16:29 +0900

    feat: nextハンズオン

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
+const Index = () => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link 
+          href={
+            'https://fonts.googleapis.com/css2' +
+            '?family=Noto+Sans+JP:wght@400;500;700' +
+            '&display=swap'
+          }
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+export default Index;
